c25045d9d8a149857c7077a9ae2bc7aa35196d41
Ivan Danin <67906259+ivandanin@users.noreply.github.com>
2021-02-15 12:38:13 +0200

    Add files via upload

---
+++ Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}, Age: {1}, Grade: {2}", name, age, grade.ToString("F"));
+
+        }
+    }
+}
